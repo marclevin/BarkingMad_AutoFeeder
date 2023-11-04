1f2e8bff22e7ade347c7117c449b893481d9719f
Marc <marclevin.sa@gmail.com>
2023-11-04 12:20:04 +0200

    Formatting.

--- background.js
+++ background.js
@@ -1,25 +1,28 @@
-local_storage_keys = {'FeedDaily': true, 'fedLast': new Date().toISOString(),  'FedToday': false, 'FeedCount': 0,'sendNotifications': true}
-
+local_storage_keys = {
+  FeedDaily: true,
+  fedLast: new Date().toISOString(),
+  FedToday: false,
+  FeedCount: 0,
+  sendNotifications: true,
+};
 
 chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === 'install') {
-      // Set up default things here
-      chrome.storage.local.set(local_storage_keys);
-      attempt_click();
-    }
-
-  });
+  if (details.reason === "install") {
+    // Set up default things here
+    chrome.storage.local.set(local_storage_keys);
+    attempt_click();
+  }
+});
 
 // When the browser starts, check if we've fed today, if not, we need to feed
-chrome.storage.local.get(['FeedDaily'], function(result) {
+chrome.storage.local.get(["FeedDaily"], function (result) {
   if (!result.FeedDaily) {
     return;
-  } else
-  {
-chrome.runtime.onStartup.addListener(() => {
-    chrome.storage.local.get(['FedToday', 'fedLast'], function(result) {
+  } else {
+    chrome.runtime.onStartup.addListener(() => {
+      chrome.storage.local.get(["FedToday", "fedLast"], function (result) {
         if (!result.FedToday) {
-            attempt_click();
+          attempt_click();
         }
         // check if it's been 24 hours since we last fed
         var fedLast = new Date(result.fedLast);
@@ -27,21 +30,22 @@ chrome.runtime.onStartup.addListener(() => {
         var diff = now - fedLast;
         var diffHours = Math.floor(diff / 1000 / 60 / 60);
         if (diffHours >= 24) {
-            chrome.storage.local.set({'FedToday': false});
-            attempt_click();
+          chrome.storage.local.set({ FedToday: false });
+          attempt_click();
         }
+      });
     });
+  }
 });
-}});
 
 // Set up an alarm to run the script every 24 hours
-chrome.alarms.create('feedAlarm', { periodInMinutes: 24 * 60 });
+chrome.alarms.create("feedAlarm", { periodInMinutes: 24 * 60 });
 
 // Add a listener for the alarm
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === 'feedAlarm') {
+  if (alarm.name === "feedAlarm") {
     // Set FedToday to false.
-    chrome.storage.local.set({'FedToday': false});
+    chrome.storage.local.set({ FedToday: false });
     attempt_click();
   }
 });
@@ -53,82 +57,89 @@ function attempt_click() {
   // Wait till the tab is loaded using the tab's promise.
   tab.then((tab) => {
     execute_click_script(tab.id).then(() => {
-        chrome.tabs.remove(tab.id);
-        });
+      chrome.tabs.remove(tab.id);
+    });
   });
-
 }
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if (request.greeting === "attempt_click") {
-        attempt_click();
-      }
-    }
-  );
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.greeting === "attempt_click") {
+    attempt_click();
+  }
+});
 
 function send_fed_success() {
-  if (!chrome.storage.local.get(['sendNotifications'], function(result) {return result.sendNotifications})) {
+  if (
+    !chrome.storage.local.get(["sendNotifications"], function (result) {
+      return result.sendNotifications;
+    })
+  ) {
     return;
   }
-  console.log("Sending notification")
-  chrome.notifications.create('BMAutoFeeder', {
-    type: 'basic',
-    message: 'You contributed another half bowl!',
-    title: "BarkingMad AutoFeeder",
-    iconUrl: './images/dog128.png'
-}, function(notificationId) {
-  console.log(chrome.runtime.lastError);
-});
+  console.log("Sending notification");
+  chrome.notifications.create(
+    "BMAutoFeeder",
+    {
+      type: "basic",
+      message: "You contributed another half bowl!",
+      title: "BarkingMad AutoFeeder",
+      iconUrl: "./images/dog128.png",
+    },
+    function (notificationId) {
+      console.log(chrome.runtime.lastError);
+    }
+  );
 }
 
 function execute_click_script(tabId) {
-    return chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        function: feed_function,
-    }).then((result) => {
-        if (result[0].result === -1) {
-            console.log("Already fed today");
-        } else if (result[0].result === -2) {
-            console.log("Couldn't find the link");
-        } else {
-            send_fed_success();
-            handle_update_locals();
-
-        }
-
-    }).catch((err) => {
-        console.log(err);
-    }
-    );
+  return chrome.scripting
+    .executeScript({
+      target: { tabId: tabId },
+      function: feed_function,
+    })
+    .then((result) => {
+      if (result[0].result === -1) {
+        console.log("Already fed today");
+      } else if (result[0].result === -2) {
+        console.log("Couldn't find the link");
+      } else {
+        send_fed_success();
+        handle_update_locals();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function handle_update_locals() {
-  if (!chrome.storage.local.get(['FedToday'], function(result) {return result.FedToday})) {
-    chrome.storage.local.set({'FedToday': true});
-    chrome.storage.local.get(['FeedCount'], function(result) {
-        var newCount = (result.FeedCount || 0) + 1;
-        chrome.storage.local.set({'FeedCount': newCount});
+  if (
+    !chrome.storage.local.get(["FedToday"], function (result) {
+      return result.FedToday;
+    })
+  ) {
+    chrome.storage.local.set({ FedToday: true });
+    chrome.storage.local.get(["FeedCount"], function (result) {
+      var newCount = (result.FeedCount || 0) + 1;
+      chrome.storage.local.set({ FeedCount: newCount });
     });
-    chrome.storage.local.set({'fedLast': new Date().toISOString()});
-}
+    chrome.storage.local.set({ fedLast: new Date().toISOString() });
+  }
 }
 
-function feed_function()
-{
-  const ALREADY_FED = -1
-  const NOT_FOUND = -2
+function feed_function() {
+  const ALREADY_FED = -1;
+  const NOT_FOUND = -2;
   const link = document.getElementById("clicktofeedlink");
   if (link) {
     if (link.classList.contains("nomoreclick")) {
       return ALREADY_FED;
     }
-      // check if the class is set to "nomoreclick", if it is, close the tab
-      while (!link.classList.contains("nomoreclick")) {
-          link.click();
-      }
-      
+    // check if the class is set to "nomoreclick", if it is, close the tab
+    while (!link.classList.contains("nomoreclick")) {
+      link.click();
+    }
   } else {
-      return NOT_FOUND;
+    return NOT_FOUND;
   }
 }
--- popup.js
+++ popup.js
@@ -1,51 +1,49 @@
-window.onload = function() {
-    chrome.storage.local.get(['FeedDaily', 'sendNotifications', 'FedToday'], function(result) {
-        document.getElementById('flexSwitchCheckChecked1').checked = result.FeedDaily;
-        document.getElementById('flexSwitchCheckChecked2').checked = result.sendNotifications;
-        if (result.FedToday) {
-            document.getElementById('fed_today').innerHTML = "You've already contributed today!";
-        }
-    });
-};
-
-document.getElementById('flexSwitchCheckChecked1').addEventListener('change', function() {
-    chrome.storage.local.set({ 'FeedDaily': this.checked });
-});
-
-document.getElementById('flexSwitchCheckChecked2').addEventListener('change', function() {
-    chrome.storage.local.set({ 'sendNotifications': this.checked });
-});
-// Set the component with ID doggos_fed to the number of dogs fed
-chrome.storage.local.get(['FeedCount'], function(result) {
-    if (result.FeedCount === 0) {
-    document.getElementById('doggos_fed').innerHTML = (`You haven't contributed any bowls yet!`);
-    return;
-    }
-    let half_bowls = convert_half_formatted(result.FeedCount);
-    if (result.FeedCount === 1) {
-        document.getElementById('doggos_fed').innerHTML = (`You've contributed half a bowl of food so far!`);
-        return;
-    }
-    if (result.FeedCount === 2) {
-        document.getElementById('doggos_fed').innerHTML = (`You've contributed 1 bowl of food so far!`);
-        return;
-    }
-    // if we have a bowl and a half, let's display it as N and a half bowls
-    if (half_bowls % 1 === 0.5) {
-        let int_bowls = Math.floor(half_bowls);
-        document.getElementById('doggos_fed').innerHTML = (`You've contributed ${int_bowls} and a half bowls of food so far!`);
-        return;
-    }
-    document.getElementById('doggos_fed').innerHTML = (`You've contributed ${half_bowls} bowls of food so far!`);
-});
-
-document.getElementById('feed_doggos').addEventListener('click', function() {
-    // Run attempt click from background.js
-    chrome.runtime.sendMessage({greeting: "attempt_click"});
-});
-
-function convert_half_formatted(number)
-{
+// Function to convert number to half and format it
+function convertHalfFormatted(number) {
     return (number / 2).toFixed(1);
-}
-
+  }
+  
+  // Function to handle window load
+  window.onload = function () {
+    chrome.storage.local.get(["FeedDaily", "sendNotifications", "FedToday"], function (result) {
+      document.getElementById("flexSwitchCheckChecked1").checked = result.FeedDaily;
+      document.getElementById("flexSwitchCheckChecked2").checked = result.sendNotifications;
+  
+      if (result.FedToday) {
+        document.getElementById("fed_today").innerHTML = "You've already contributed today!";
+      }
+    });
+  };
+  
+  // Event listeners for switches
+  document.getElementById("flexSwitchCheckChecked1").addEventListener("change", function () {
+    chrome.storage.local.set({ FeedDaily: this.checked });
+  });
+  
+  document.getElementById("flexSwitchCheckChecked2").addEventListener("change", function () {
+    chrome.storage.local.set({ sendNotifications: this.checked });
+  });
+  
+  // Fetch and display the feed count
+  chrome.storage.local.get(["FeedCount"], function (result) {
+    let feedCountMessage = "You haven't contributed any bowls yet!";
+    let halfBowls = convertHalfFormatted(result.FeedCount);
+  
+    if (result.FeedCount === 1) {
+      feedCountMessage = "You've contributed half a bowl of food so far!";
+    } else if (result.FeedCount === 2) {
+      feedCountMessage = "You've contributed 1 bowl of food so far!";
+    } else if (halfBowls % 1 === 0.5) {
+      let intBowls = Math.floor(halfBowls);
+      feedCountMessage = `You've contributed ${intBowls} and a half bowls of food so far!`;
+    } else {
+      feedCountMessage = `You've contributed ${halfBowls} bowls of food so far!`;
+    }
+  
+    document.getElementById("doggos_fed").innerHTML = feedCountMessage;
+  });
+  
+  // Event listener for feed button
+  document.getElementById("feed_doggos").addEventListener("click", function () {
+    chrome.runtime.sendMessage({ greeting: "attempt_click" });
+  });
